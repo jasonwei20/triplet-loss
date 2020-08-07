8489123679087868ed3d4f084bfcb5e9f6530370
Jason Wei <jason.weng.wei@gmail.com>
2020-08-07 13:50:34 -0400

    add more CL two-step experiments

--- triplet_ap_cl_twostep.py
+++ triplet_ap_cl_twostep.py
@@ -3,22 +3,18 @@ from utils import common, configuration, triplet_cl_methods
 if __name__ == "__main__":
 
     cfg_json_list = [
-        # "config/triplet_ap/cl_twostep/covidcat_nc10_a01.json",
-        # "config/triplet_ap/cl_twostep/covidcat_nc10_a02.json",
-        # "config/triplet_ap/cl_twostep/covidcat_nc10_a03.json",
-        # "config/triplet_ap/cl_twostep/covidcat_nc10_a04.json",
-        # "config/triplet_ap/cl_twostep/covidclu_nc3_a01.json",
-        # "config/triplet_ap/cl_twostep/covidclu_nc3_a02.json",
-        # "config/triplet_ap/cl_twostep/covidclu_nc3_a03.json",
-        # "config/triplet_ap/cl_twostep/covidclu_nc3_a04.json",
-        # "config/triplet_ap/cl_twostep/huff_nc10_a01.json",
-        # "config/triplet_ap/cl_twostep/huff_nc10_a02.json",
-        # "config/triplet_ap/cl_twostep/huff_nc10_a03.json",
-        # "config/triplet_ap/cl_twostep/huff_nc10_a04.json",
-        "config/triplet_ap/cl_twostep/fewrel_nc10_a04.json",
-        "config/triplet_ap/cl_twostep/fewrel_nc10_a03.json",
-        "config/triplet_ap/cl_twostep/fewrel_nc10_a02.json",
-        "config/triplet_ap/cl_twostep/fewrel_nc10_a01.json",
+        # "config/triplet_ap/cl_twostep/covidcat_nc10_a05.json",
+        # "config/triplet_ap/cl_twostep/covidcat_nc10_a005.json",
+        # "config/triplet_ap/cl_twostep/covidclu_nc3_a05.json",
+        # "config/triplet_ap/cl_twostep/covidclu_nc3_a005.json",
+        # "config/triplet_ap/cl_twostep/huff_nc5_a05.json",
+        # "config/triplet_ap/cl_twostep/huff_nc5_a005.json",
+        # "config/triplet_ap/cl_twostep/huff_nc10_a05.json",
+        # "config/triplet_ap/cl_twostep/huff_nc10_a005.json",
+        # "config/triplet_ap/cl_twostep/fewrel_nc5_a05.json",
+        # "config/triplet_ap/cl_twostep/fewrel_nc5_a005.json",
+        # "config/triplet_ap/cl_twostep/fewrel_nc10_a05.json",
+        # "config/triplet_ap/cl_twostep/fewrel_nc10_a005.json",
     ]
 
     for cfg_json in cfg_json_list:
