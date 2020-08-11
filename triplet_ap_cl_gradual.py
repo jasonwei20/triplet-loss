e1da793ad7114433a779f974e79ee13a1114555e
Jason Wei <jason.weng.wei@gmail.com>
2020-08-10 20:58:33 -0400

    add cl gradual experiments for amzn

--- triplet_ap_cl_gradual.py
+++ triplet_ap_cl_gradual.py
@@ -3,18 +3,20 @@ from utils import common, configuration, triplet_cl_methods
 if __name__ == "__main__":
 
     cfg_json_list = [
-        "config/triplet_ap/cl_gradual/covidcat_nc10_a.json",
-        "config/triplet_ap/cl_gradual/covidcat_nc10_b.json",
-        "config/triplet_ap/cl_gradual/covidclu_nc3_a.json",
-        "config/triplet_ap/cl_gradual/covidclu_nc3_b.json",
-        "config/triplet_ap/cl_gradual/huff_nc5_a.json",
-        "config/triplet_ap/cl_gradual/huff_nc5_b.json",
-        "config/triplet_ap/cl_gradual/huff_nc10_a.json",
-        "config/triplet_ap/cl_gradual/huff_nc10_b.json",
-        "config/triplet_ap/cl_gradual/fewrel_nc5_a.json",
-        "config/triplet_ap/cl_gradual/fewrel_nc5_b.json",
-        "config/triplet_ap/cl_gradual/fewrel_nc10_a.json",
-        "config/triplet_ap/cl_gradual/fewrel_nc10_b.json",
+        # "config/triplet_ap/cl_gradual/covidcat_nc10_a.json",
+        # "config/triplet_ap/cl_gradual/covidcat_nc10_b.json",
+        # "config/triplet_ap/cl_gradual/covidclu_nc3_a.json",
+        # "config/triplet_ap/cl_gradual/covidclu_nc3_b.json",
+        # "config/triplet_ap/cl_gradual/huff_nc5_a.json",
+        # "config/triplet_ap/cl_gradual/huff_nc5_b.json",
+        # "config/triplet_ap/cl_gradual/huff_nc10_a.json",
+        # "config/triplet_ap/cl_gradual/huff_nc10_b.json",
+        # "config/triplet_ap/cl_gradual/fewrel_nc5_a.json",
+        # "config/triplet_ap/cl_gradual/fewrel_nc5_b.json",
+        # "config/triplet_ap/cl_gradual/fewrel_nc10_a.json",
+        # "config/triplet_ap/cl_gradual/fewrel_nc10_b.json",
+        # "config/triplet_ap/cl_gradual/amzn_nc3_a.json",
+        # "config/triplet_ap/cl_gradual/amzn_nc3_b.json",
     ]
 
     for cfg_json in cfg_json_list:
