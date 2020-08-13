fdb2b0802ec6a4e76204b2430908751e0b622468
Jason Wei <jason.weng.wei@gmail.com>
2020-08-13 00:35:10 -0400

    add bt twostep

---
+++ triplet_ap_bt_twostep.py
@@ -0,0 +1,19 @@
+from utils import common, configuration, triplet_cl_methods
+
+if __name__ == "__main__":
+
+    cfg_json_list = [
+        # "config/triplet_ap/bt_twostep/amzn_nc3_a01.json",
+        # "config/triplet_ap/bt_twostep/covidclu_nc3_a01.json",
+        # "config/triplet_ap/bt_twostep/fewrel_nc5_a01.json",
+        # "config/triplet_ap/bt_twostep/fewrel_nc10_a01.json",
+        # "config/triplet_ap/bt_twostep/huff_nc5_a01.json",
+        # "config/triplet_ap/bt_twostep/huff_nc10_a01.json",
+    ]
+
+    for cfg_json in cfg_json_list:
+
+        cfg = configuration.triplet_ap_cl_config.from_json(cfg_json); print(f"config from {cfg_json}")
+        common.set_random_seed(cfg.seed_num)
+
+        triplet_cl_methods.train_eval_cl_model(cfg)
