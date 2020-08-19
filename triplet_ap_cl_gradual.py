62babddc6fe8b1f2adca945899d6e218470aff33
Jason Wei <jason.weng.wei@gmail.com>
2020-08-19 01:26:01 -0400

    add some more experiments

--- triplet_ap_cl_gradual.py
+++ triplet_ap_cl_gradual.py
@@ -13,11 +13,13 @@ if __name__ == "__main__":
         # "config/triplet_ap/cl_gradual/huff_nc10_b.json",
         # "config/triplet_ap/cl_gradual/fewrel_nc5_a.json",
         # "config/triplet_ap/cl_gradual/fewrel_nc5_b.json",
+        # "config/triplet_ap/cl_gradual/fewrel_nc5_b2.json",
         # "config/triplet_ap/cl_gradual/fewrel_nc10_a.json",
         # "config/triplet_ap/cl_gradual/fewrel_nc10_b.json",
+        # "config/triplet_ap/cl_gradual/fewrel_nc10_c.json",
         # "config/triplet_ap/cl_gradual/amzn_nc3_a.json",
         # "config/triplet_ap/cl_gradual/amzn_nc3_b.json",
-        "config/triplet_ap/cl_gradual/amzn_nc3_b2.json",
+        # "config/triplet_ap/cl_gradual/amzn_nc3_b2.json",
         # "config/triplet_ap/cl_gradual/amzn_nc3_c.json",
     ]
 
