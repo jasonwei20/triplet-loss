d71de3a341260bba46dcf430276656fd407bc276
Jason Wei <jason.weng.wei@gmail.com>
2020-08-19 14:37:07 -0400

    change plots code

--- triplet_ap_plots.py
+++ triplet_ap_plots.py
@@ -2,56 +2,66 @@ from utils import common, configuration, triplet_methods, triplet_cl_methods
 
 if __name__ == "__main__":
 
-    cfg_json_list_vanilla = [
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s0.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s1.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s2.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s3.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s4.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s5.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s6.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s7.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s8.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s9.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s10.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s11.json",
-        # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s12.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s0.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s1.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s2.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s3.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s4.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s0.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s1.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s2.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s3.json",
-        # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s4.json",
-    ]
-
-    for cfg_json in cfg_json_list_vanilla:
-
-        cfg = configuration.triplet_ap_config.from_json(cfg_json); print(f"config from {cfg_json}")
-        common.set_random_seed(cfg.seed_num)
-
-        triplet_methods.train_eval_model(cfg)
-
-
-    # cfg_json_list = [
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s0.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s1.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s2.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s3.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s4.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s0.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s1.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s2.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s3.json",
-    #     # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s4.json",
+    # cfg_json_list_vanilla = [
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s0.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s1.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s2.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s3.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s4.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s5.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s6.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s7.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s8.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s9.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s10.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s11.json",
+    #     # "config/triplet_ap/eda_alpha_output/vanilla_huff_nc5_s12.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a02_s0.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a02_s1.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a02_s2.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a02_s3.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a02_s4.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s0.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s1.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s2.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s3.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a03_s4.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s0.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s1.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s2.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s3.json",
+    #     # "config/triplet_ap/eda_alpha_output/eda_huff_nc5_a04_s4.json",
     # ]
 
-    # for cfg_json in cfg_json_list:
+    # for cfg_json in cfg_json_list_vanilla:
 
-    #     cfg = configuration.triplet_ap_cl_config.from_json(cfg_json); print(f"config from {cfg_json}")
+    #     cfg = configuration.triplet_ap_config.from_json(cfg_json); print(f"config from {cfg_json}")
     #     common.set_random_seed(cfg.seed_num)
 
-    #     triplet_cl_methods.train_eval_cl_model(cfg)
+    #     triplet_methods.train_eval_model(cfg)
+
+
+    cfg_json_list = [
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a02_s0.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a02_s1.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a02_s2.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a02_s3.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a02_s4.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s0.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s1.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s2.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s3.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a03_s4.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s0.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s1.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s2.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s3.json",
+        # "config/triplet_ap/eda_alpha_output/edacl_huff_nc5_a04_s4.json",
+    ]
+
+    for cfg_json in cfg_json_list:
+
+        cfg = configuration.triplet_ap_cl_config.from_json(cfg_json); print(f"config from {cfg_json}")
+        common.set_random_seed(cfg.seed_num)
+
+        triplet_cl_methods.train_eval_cl_model(cfg)
